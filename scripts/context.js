1919894b3f11d58dc940422a3692ee4d4325f4ce
Joni De Borger <joni.de.borger@ehb.be>
2023-10-19 10:16:22 +0200

    fixed height

--- scripts/context.js
+++ scripts/context.js
@@ -10,7 +10,7 @@ export function setupCanvasContext() {
     //get a reference to the canvas element and configure it to fill the whole window
     let canvas = document.querySelector("canvas");
     canvas.width = window.innerWidth;
-    canvas.height = 300;
+    canvas.height = window.innerHeight;
     context = canvas.getContext("2d");
 }
 
